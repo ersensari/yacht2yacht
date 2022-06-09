91c1b53685e2a3db24ad52545c0f979af7668e47
Ersen SARI <ersensari@gmail.com>
2022-06-09 23:24:14 +1000

    i18n

--- www/server/index.ts
+++ www/server/index.ts
@@ -3,7 +3,7 @@ import compression from 'compression'
 import { createPageRenderer } from 'vite-plugin-ssr'
 import type { PageContext } from '~/types'
 import * as vite from 'vite'
-// import { extractLocale } from '../i18n/locales'
+import { extractLocale } from '../i18n/locales'
 
 const isProduction = process.env.NODE_ENV === 'production'
 const root = `${__dirname}/..`
@@ -34,15 +34,15 @@ async function startServer() {
 
   app.get('*', async (req, res, next) => {
     let url = req.originalUrl
-    // if (url.startsWith('/manifest')) return next()
+    if (url.startsWith('/manifest')) return next()
 
-    // let { locale, urlWithoutLocale } = extractLocale(url)
-    // url = urlWithoutLocale
+    let { locale, urlWithoutLocale } = extractLocale(url)
+    url = urlWithoutLocale
 
     // console.log('server.ts:', locale, url);
 
 
-    const pageContextInit = { url }
+    const pageContextInit = { url, locale }
     const pageContext = await renderPage(pageContextInit)
 
     const { httpResponse } = pageContext
